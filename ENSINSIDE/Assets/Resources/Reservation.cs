e6038585f6c37c15c99b00d189868fb5cb2251ab
Levidre <45667043+Levidre@users.noreply.github.com>
2019-06-10 01:44:49 +0200

    Add files via upload

---
+++ ENSINSIDE/Assets/Resources/Reservation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public abstract class Reservation : MonoBehaviour
+{
+
+	
+	public abstract bool isActive(DateTime date);
+	
+	public abstract User getUser();
+	
+	public abstract string getDescript();
+	
+	public abstract void setUser(User u);
+	
+	public abstract float getRoom();
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
